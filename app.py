2c984b0deebeeab26640d0c10d306482ddd29030
Tim <t1225947@gmail.com>
2024-05-31 12:24:49 +0800

    commit

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def callback():
     app.logger.info("Request body: " + body)
         # handle webhook body
     try:
-        handler.handle(body[:3], signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
     return 'OK'
@@ -56,7 +56,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    line_bot_api.reply_message(event.reply_token, message[:3])
 
 # 處理訊息
 # @handler.add(MessageEvent, message=TextMessage)
