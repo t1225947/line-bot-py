763d867fac7ca20a24e1db5e516ac288a0a135ed
Tim <t1225947@gmail.com>
2024-06-01 07:35:54 +0800

    Update app.py
    20240601

--- app.py
+++ app.py
@@ -55,9 +55,18 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if (event.message.text[:2] == "O:"):
-       message = TextSendMessage(text=event.source.groupId)
-       line_bot_api.reply_message(event.reply_token, message)
+    if event.message.text == 'ID?' or event.message.text == 'id?':
+        User_ID = TextMessage(text=event.source.user_id)
+        line_bot_api.reply_message(event.reply_token, User_ID)
+       
+    elif event.message.text == 'GroupID?':
+        Group_ID = TextMessage(text=event.source.group_id)
+        line_bot_api.reply_message(event.reply_token, Group_ID)
+       
+        
+   # if (event.message.text[:2] == "O:"):
+    #   message = TextSendMessage(text=event.source.groupId)
+    #   line_bot_api.reply_message(event.reply_token, message)
 
 # 處理訊息
 # @handler.add(MessageEvent, message=TextMessage)
