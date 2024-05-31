cf4536609ab34865f05c52dd19c742a46dece1af
Tim <t1225947@gmail.com>
2024-05-31 12:29:24 +0800

    commit

--- app.py
+++ app.py
@@ -55,8 +55,9 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message[:3])
+   # if (event.message.text == "OTA"):
+    message = TextSendMessage(text=event.message.text[:3])
+    line_bot_api.reply_message(event.reply_token, message)
 
 # 處理訊息
 # @handler.add(MessageEvent, message=TextMessage)
