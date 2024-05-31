59ce15632520f59cf0af8fa1737f09c101139a5a
Tim <t1225947@gmail.com>
2024-05-31 09:47:39 +0800

    commit

--- app.py
+++ app.py
@@ -42,13 +42,15 @@ def callback():
     signature = request.headers['X-Line-Signature']
     # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
+    if (body[:3] == "OTA"):        
+       app.logger.info("Request body: " + body)
+        # handle webhook body
+       try:
+          handler.handle(body, signature)
+       except InvalidSignatureError:
         abort(400)
-    return 'OK'
+       return 'OK'
+    
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
