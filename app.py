93075c0f63c2dd9332583273ecd95693224c590e
Tim <t1225947@gmail.com>
2024-05-31 11:10:49 +0800

    commit

--- app.py
+++ app.py
@@ -43,10 +43,10 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
    # if (body[:3] == "OTA"):        
-    app.logger.info("Request body: " + body[:3])
+    app.logger.info("Request body: " + body)
         # handle webhook body
     try:
-        handler.handle(body, signature)
+        handler.handle(body[:3], signature)
     except InvalidSignatureError:
         abort(400)
     return 'OK'
